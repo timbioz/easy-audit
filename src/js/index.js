6b0bbfe3a4b2ddc7a412f159b3751d90c02b4046
timbioz <volkovtim@gmail.com>
2019-03-21 01:30:15 +0300

    Initial Commit

---
+++ src/js/index.js
@@ -0,0 +1,14 @@
+//import "bootstrap/dist/css/bootstrap"
+
+// import "../css/index.css"
+import "../scss/index.scss"
+
+
+// import {render} from "../ts/index"
+
+// import "../images/logo.png";
+// import "../images/car1.jpg";
+// import "../images/car2.jpg";
+// import "../images/car3.jpg";
+
+// import "../images/test.svg";
